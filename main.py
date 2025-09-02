b427140dd70739d61d8651f08fc5b2d1d363cfb1
zach-tur <108747741+zach-tur@users.noreply.github.com>
2025-09-02 15:48:34 -0700

    organized sql functions into new file

--- main.py
+++ main.py
@@ -3,25 +3,7 @@ import textual
 import textual_dev
 
 import sqlite3
-
-DB_PATH = os.path.join(os.path.dirname(__file__), "data", "data.db")
-os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
-
-
-def get_conn():
-    conn = sqlite3.connect(DB_PATH)
-    conn.execute("PRAGMA foreign_keys = ON")
-    return conn
-
-
-with open(
-    os.path.join(os.path.dirname(__file__), "migrations", "001_init.sql"), "r"
-) as f:
-    ddl = f.read()
-with get_conn() as conn:
-    conn.executescript(ddl)
-
-# con = sqlite3.connect("dbhere.db")
+import src.sql_handling
 
 
 def main():
